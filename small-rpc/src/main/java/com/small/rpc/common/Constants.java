d6c08f6c26477b10c8884fc0cea021d6ab03aa70
Kevin <841156602@qq.com>
2019-03-24 22:59:56 +0800

    feat:implements code by http://www.cnblogs.com/luxiaoxun/p/5272384.html

---
+++ small-rpc/src/main/java/com/small/rpc/common/Constants.java
@@ -0,0 +1,8 @@
+package com.small.rpc.common;
+
+public interface Constants {
+    int ZK_SESSION_TIMEOUT = 5000;
+
+    String ZK_REGISTRY_PATH = "/registry";
+    String ZK_DATA_PATH = ZK_REGISTRY_PATH + "/data";
+}
---
+++ small-rpc/src/main/java/com/small/rpc/registry/ServiceRegistry.java
@@ -0,0 +1,60 @@
+package com.small.rpc.registry;
+
+import com.small.rpc.common.Constants;
+import org.apache.zookeeper.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.CountDownLatch;
+
+public class ServiceRegistry {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ServiceRegistry.class);
+
+    private CountDownLatch latch = new CountDownLatch(1);
+
+    private String registryHost;
+
+    private int registryPort;
+
+    public ServiceRegistry(String registryHost, int registryPort){
+        this.registryHost = registryHost;
+        this.registryPort = registryPort;
+    }
+
+    public void register(String data) {
+        if (data != null) {
+            ZooKeeper zk = connectServer();
+            if (zk != null) {
+                createNode(zk, data);
+            }
+        }
+    }
+
+    private void createNode(ZooKeeper zk, String data) {
+        try {
+            byte[] bytes = data.getBytes();
+            String path = zk.create(Constants.ZK_DATA_PATH, bytes, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+            LOGGER.debug("create zookeeper node ({} => {})", path, data);
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+        }
+    }
+
+    private ZooKeeper connectServer(){
+        ZooKeeper zooKeeper = null;
+        try {
+            zooKeeper = new ZooKeeper(registryHost + ":" + registryPort, Constants.ZK_SESSION_TIMEOUT, new Watcher() {
+                public void process(WatchedEvent watchedEvent) {
+                    if (watchedEvent.getState() == Event.KeeperState.SyncConnected) {
+                        latch.countDown();
+                    }
+                }
+            });
+            latch.await();
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+        }
+        return zooKeeper;
+    }
+
+}
