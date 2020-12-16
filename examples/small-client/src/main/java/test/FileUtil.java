9a173e3decf3d5c067bfddb77f858f14f14fc5dd
wangguodong <wangguodong@veredholdings.com>
2020-12-16 09:42:58 +0800

    feat:升级junit version

---
+++ examples/small-client/src/main/java/test/FileUtil.java
@@ -0,0 +1,12 @@
+package test;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileUtil {
+    public static List<String> readLines(File file, String s) {
+        // TODO
+        return new ArrayList<>();
+    }
+}
